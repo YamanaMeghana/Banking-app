860ad56103af4093b739eeea21518abfdd311d58
YamanaMeghana <161587550+YamanaMeghana@users.noreply.github.com>
2024-05-14 20:23:47 +0530

    Update main.tf

--- scripts/main.tf
+++ scripts/main.tf
@@ -10,7 +10,18 @@ resource "aws_instance" "Financedeploy-server" {
     host     = self.public_ip
   }
   provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
+
+    inline = [
+      "echo 'wait to start instance'",
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https software-properties-common wget",
+      "sudo mkdir -p /etc/apt/keyrings/",
+      "sudo wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null",
+      "sudo sh -c 'echo \"deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main\" > /etc/apt/sources.list.d/grafana.list'",
+      "sudo apt-get update",
+      "sudo apt-get install -y grafana",
+      "sudo systemctl start grafana-server"
+     ]
   }
   tags = {
     Name = "Financedeploy-server"
