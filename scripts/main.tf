b01bb628b97089197da443bb3e3d236705dd8341
YamanaMeghana <161587550+YamanaMeghana@users.noreply.github.com>
2024-05-15 10:46:58 +0530

    Update main.tf

--- scripts/main.tf
+++ scripts/main.tf
@@ -20,15 +20,7 @@ resource "aws_instance" "Financedeploy-server" {
       "echo 'wait to start instance'",
       "wget -q -O gpg.key https://rpm.grafana.com/gpg.key"
       "sudo rpm --import gpg.key"
-      "[grafana]
-       name=grafana
-       baseurl=https://rpm.grafana.com
-       repo_gpgcheck=1
-       enabled=1
-       gpgcheck=1
-       gpgkey=https://rpm.grafana.com/gpg.key
-       sslverify=1
-       sslcacert=/etc/pki/tls/certs/ca-bundle.crt"
+      "[grafana]\nname=grafana\nbaseurl=https://rpm.grafana.com\nrepo_gpgcheck=1\nenabled=1\ngpgcheck=1\ngpgkey=https://rpm.grafana.com/gpg.key\nsslverify=1\nsslcacert=/etc/pki/tls/certs/ca-bundle.crt"
       "sudo dnf install grafana"
       "sudo dnf install grafana-enterprise"
      ]
