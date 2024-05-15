01c8f4ca6036ee665769b1d3293953b9974faeaf
YamanaMeghana <161587550+YamanaMeghana@users.noreply.github.com>
2024-05-15 11:11:19 +0530

    Update main.tf

--- scripts/main.tf
+++ scripts/main.tf
@@ -7,38 +7,39 @@ resource "aws_instance" "Financedeploy-server" {
     Name = "Financedeploy-server"
   }
 }
-  resource "null_resource" "install_grafana" {
+ resource "null_resource" "install_grafana" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = file("./My-gitdevops.pem")
     host        = aws_instance.Financedeploy-server.public_ip
   }
-  provisioner "remote-exec" {
 
+  provisioner "remote-exec" {
     inline = [
       "echo 'wait to start instance'",
-      "ssh -i /.ssh/My-gitdevops.pem ec2-52-91-98-158.compute-1.amazonaws.com",
-      "sudo tee /etc/yum.repos.d/grafana.repo <<EOF
-       [grafana]
-       name=grafana
-       baseurl=https://packages.grafana.com/oss/rpm
-       repo_gpgcheck=1
-       enabled=1
-       gpgcheck=1
-       gpgkey=https://packages.grafana.com/gpg.key
-       sslverify=1
-       EOF
-     "sudo yum update -y",
-     "sudo yum install grafana -y",
-     "sudo systemctl start grafana-server",
-     "sudo systemctl enable grafana-server"
-     ]
+      "sudo tee /etc/yum.repos.d/grafana.repo <<EOF",
+      "[grafana]",
+      "name=grafana",
+      "baseurl=https://packages.grafana.com/oss/rpm",
+      "repo_gpgcheck=1",
+      "enabled=1",
+      "gpgcheck=1",
+      "gpgkey=https://packages.grafana.com/gpg.key",
+      "sslverify=1",
+      "EOF",
+      "sudo yum update -y",
+      "sudo yum install grafana -y",
+      "sudo systemctl start grafana-server",
+      "sudo systemctl enable grafana-server"
+    ]
   }
+
   provisioner "local-exec" {
-        command = " echo ${aws_instance.Financedeploy-server.public_ip} > inventory "
+    command = "echo ${aws_instance.Financedeploy-server.public_ip} > inventory"
   }
-   provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-app/scripts/monitoring.yml "
-  } 
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Banking-app/scripts/monitoring.yml"
+  }
 }
